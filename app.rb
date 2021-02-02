69900ecf40e386c6edd551df4bb7339b01240902
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-02 10:45:15 +0000

    added a form for hosts to be able to list properties

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
-require 'database_setup'
+require './lib/database_setup'
 
 # Bunker class
 class Bunker < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  get '/add_property' do
+    erb :add_property
   end
 
   run! if app_file == $0
---
+++ spec/features/add_property_spec.rb
@@ -0,0 +1,9 @@
+feature 'add a property' do
+  scenario 'can enter property information' do
+    visit ('/add_property')
+    fill_in :name, with: "nuclear_bunker"
+    fill_in :description, with: "Designed for up to 600 military and civilian personnel, possibly even the Prime Minister"
+    fill_in :price, with: 10.00
+    click_button "List your bunker!"
+  end
+end
