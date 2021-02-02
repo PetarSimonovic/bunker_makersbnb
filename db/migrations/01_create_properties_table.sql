4dcb33096d830ab14a1f703b16a529fc00bea0f0
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-02 10:14:27 +0000

    fixing the mirgration file

--- db/migrations/01_create_properties_table.sql
+++ db/migrations/01_create_properties_table.sql
@@ -1,3 +1,1 @@
-CREATE TABLE properties(id SERIAL PRIMARY KEY, nam
-e VARCHAR(60), description VARCHAR(200), price DECIMAL(8,2), ava
-ilability VARCHAR(25));
+CREATE TABLE properties(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(200), price DECIMAL(8,2), availability VARCHAR(25));
