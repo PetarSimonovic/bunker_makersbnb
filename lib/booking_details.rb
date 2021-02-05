73860e68733e6396a795e08dcb91786e1d7f6bb4
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-05 10:50:44 +0000

    booking ref displays

--- lib/booking_details.rb
+++ lib/booking_details.rb
@@ -3,14 +3,15 @@ require './lib/property'
 
 class BookingDetails
 
-  attr_reader :property_id, :host_id, :name, :status, :guest_id
+  attr_reader :property_id, :host_id, :name, :status, :guest_id, :booking_id
 
-  def initialize(property_id:, host_id:, name:, status:, guest_id:)
+  def initialize(property_id:, host_id:, name:, status:, guest_id:, booking_id:)
     @property_id = property_id
     @host_id = host_id
     @name = name
     @status = status
     @guest_id = guest_id
+    @booking_id = booking_id
   end
 
   def self.booking_details(user_id:)
@@ -20,10 +21,10 @@ class BookingDetails
         connection = PG.connect(dbname: 'bunker')
     end
 
-    details = connection.exec("SELECT properties.property_id, properties.host_id, name, status, guest_id FROM properties INNER JOIN bookings ON bookings.property_id = properties.property_id WHERE properties.host_id = '#{user_id.to_i}';")
+    details = connection.exec("SELECT properties.property_id, properties.host_id, name, status, guest_id, booking_id FROM properties INNER JOIN bookings ON bookings.property_id = properties.property_id WHERE properties.host_id = '#{user_id.to_i}';")
 
     details.map do |detail|
-      BookingDetails.new(property_id: detail['property_id'], name: detail['name'], status: detail['status'],  host_id: detail['host_id'], guest_id: detail['guest_id'])
+      BookingDetails.new(property_id: detail['property_id'], name: detail['name'], status: detail['status'],  host_id: detail['host_id'], guest_id: detail['guest_id'], booking_id: detail['booking_id'])
     end
   end
 
